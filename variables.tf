68e101774469ec84b4ef527207dfd450aa87a265
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-02 22:29:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "availability_zones" {
   type        = list(string)
   description = "List of availability zones for VPC subnets"
+  default= ["us-east-2a", "us-east-2b"]
 }
 
 variable "region" {
   type        = string
   description = "AWS region"
+   default = "us-east-2"
 }
