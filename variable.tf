1ac7411b8a7aec5a943db919becfd9d05d80dba5
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-03 23:02:39 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,11 @@
 variable "name1" {
 type        = string
 description = "name of the env"
+  default="exam"
 }
 
 variable "arn" {
 type        = string
 description = "name of arn"
+  default="arn:aws:iam::863309687429:role/service-role/AmazonMWAA-MyAirflowEnvironment"
 }
