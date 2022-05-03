1010086b2814fa76f209ce6f246bc13bae95f265
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-03 21:56:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ resource "aws_mwaa_environment" "example" {
   name               = "example"
 
   network_configuration {
-    security_group_ids = ["sg-04f0b6e88fcd2b6de","sg-07f4377f13eefbdc6"]
-    subnet_ids         = ["sg-07f4377f13eefbdc6"]
+    security_group_ids = ["sg-07f4377f13eefbdc6"]
+    subnet_ids         = ["subnet-05244b86b15d97348","subnet-083f60308cc643012"]
   }
 
   source_bucket_arn = "arn:aws:s3:::airflow197805"
