5b91bf1d7068e5ca43f6baceeb1206a7c923ffb4
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-03 22:53:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
 
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-
 resource "aws_mwaa_environment" "example" {
    dag_s3_path        = "dags/"
-  execution_role_arn = "arn:aws:iam::863309687429:role/service-role/AmazonMWAA-MyAirflowEnvironment"
-  name               = "example"
+   execution_role_arn = var.arn
+   name               = var.name
 
   network_configuration {
     security_group_ids = ["sg-07f4377f13eefbdc6"]
