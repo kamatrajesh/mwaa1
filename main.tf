ffebb48210e2e5c63b74fbe8d01c4eda78fcabfc
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-03 21:07:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "aws_mwaa_environment" "example" {
-  dag_s3_path        = "dags/"
-  execution_role_arn = aws_iam_role.example.arn
-  name               = "example"
-
-  network_configuration {
-    security_group_ids = [aws_security_group.example.id]
-    subnet_ids         = aws_subnet.private[*].id
-  }
-
-  source_bucket_arn = aws_s3_bucket.example.arn
+module "airflow" {
+  source = "idealo/mwaa/aws"
+  version = "2.2.2"
+  account_id  "863309687429"
+  environmentname = "MyEnvironment_terra"
+  internet_gateway_id = "igw-08ecca2ea7c88702a"
+  private_subnet_cidrs = ["10.192.20.0/24","10.192.21.0/24"] # depending on your vpc ip range
+  public_subnet_cidrs = ["10.192.10.0/24","10.192.11.0/24"] # depending on your vpc ip range
+  region = "us-east-1"
+  source_bucket_arn = "arn:aws:s3:::airflow197805"
+  vpc_id = "vpc-06179917ff299190d"
 }
